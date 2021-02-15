d57d707ebfb088c0622f5a5b090df9432a29c48c
Rafael Cavalcante <rafael.marques.cavalcante@gmail.com>
2021-02-15 09:09:25 -0300

    introducing logout resolver

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { PostResolver } from './resolvers/post';
 
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
+import { COOKIE_NAME } from "./constants";
 import microConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
@@ -34,7 +35,7 @@ const main = async () => {
 
   app.use(
     session({
-      name: 'qid',
+      name: COOKIE_NAME,
       store: new RedisStore({ client: redisClient, disableTouch: true }),
       cookie: {
         maxAge: 1000 * 3600 * 24,
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,3 +1,4 @@
+import { COOKIE_NAME } from './../constants';
 import {
   Resolver,
   Mutation,
@@ -149,4 +150,25 @@ export class UserResolver {
       user,
     };
   }
+
+  
+  
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err: any) => {
+        console.log(err)
+        res.clearCookie(COOKIE_NAME);
+        if (err) {
+          console.log(err);
+          resolve(false);
+          return;
+        }
+
+        resolve(true);
+      })
+    );
+  }
+
+
 }
