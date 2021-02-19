4f41140f018cebb7c7ae157a119c092d950fee3b
Rafael Cavalcante <rafael.marques.cavalcante@gmail.com>
2021-02-19 11:30:09 -0300

    using better orm

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,71 @@
-import { EntityManager } from '@mikro-orm/postgresql';
-import { User } from './entities/User';
-import { MyContext } from './types';
-import { UserResolver } from './resolvers/user';
-import 'reflect-metadata'
-import { PostResolver } from './resolvers/post';
-
-import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import { COOKIE_NAME } from "./constants";
-import microConfig from "./mikro-orm.config";
+import "reflect-metadata";
+import { __prod__, COOKIE_NAME } from "./constants";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
-
-import Redis from 'ioredis';
-import session from 'express-session';
-import connectRedis from 'connect-redis'
-import cors from 'cors'
-import { sendEmail } from './utils/sendEmail';
-
-
-
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+import Redis from "ioredis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import cors from "cors";
+import { createConnection } from "typeorm";
+import { Post } from "./entities/Post";
+import { User } from "./entities/User";
 
 const main = async () => {
-  const orm = await MikroORM.init(microConfig);
-
-  await orm.getMigrator().up()  
+  const conn = await createConnection({
+    type: "postgres",
+    database: "donow",
+    username: "newboy",
+    password: "eueumesmo!",
+    logging: true,
+    synchronize: true,
+    entities: [Post, User],
+  });
 
   const app = express();
 
-  let RedisStore = connectRedis(session)
-  let redis = new Redis()
-
-  app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true
-  }))
+  const RedisStore = connectRedis(session);
+  const redis = new Redis();
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
   
-
   app.use(
     session({
       name: COOKIE_NAME,
-      store: new RedisStore({ client: redis, disableTouch: true }),
+      store: new RedisStore({
+        client: redis,
+        disableTouch: true,
+      }),
       cookie: {
-        maxAge: 1000 * 3600 * 24,
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
         httpOnly: true,
-        sameSite: 'lax',
-        secure: __prod__
+        sameSite: "lax", // csrf
+        secure: __prod__, // cookie only works in https
       },
       saveUninitialized: false,
-      secret: 'keyboard cat',
+      secret: "qowiueojwojfalksdjoqiwueo",
       resave: false,
     })
-  )
+  );
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [UserResolver, PostResolver],
+      resolvers: [PostResolver, UserResolver],
       validate: false,
     }),
-    context: ({ req, res }): MyContext => ({ em: orm.em, req, res, redis })
+    context: ({ req, res }) => ({ req, res, redis }),
   });
 
-  apolloServer.applyMiddleware({ 
-  app,
-  cors: false 
-});
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
 
   app.listen(4000, () => {
     console.log("server started on localhost:4000");
